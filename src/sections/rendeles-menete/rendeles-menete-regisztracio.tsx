215fa1b2e06649c900ae3b8e5ab6c7130486d246
anonline <uerik26@gmail.com>
2025-06-27 12:23:38 +0200

    F2FV3-33 | feat: Update navigation paths and add 'Rendelés menete' section with icon

--- src/sections/rendeles-menete/rendeles-menete-regisztracio.tsx
+++ src/sections/rendeles-menete/rendeles-menete-regisztracio.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography} from "@mui/material";
+import F2FIcons from "src/components/f2ficons/f2ficons";
 import { Image } from "src/components/image";
 
 export default function RendelesMeneteRegisztracio() {
@@ -18,7 +19,8 @@ export default function RendelesMeneteRegisztracio() {
     }
   return (
     <Box>
-      <Image src=""/>
+      <F2FIcons name="FileIcon" height={100} width={100} style={{color:'rgb(74,110,80)'}} />
+      
       <Typography sx={h1Style} component="h1" gutterBottom>Regisztráció</Typography>
       <Typography sx={textStyle} gutterBottom>
         Akár étteremként, akár magánszemélyként rendelsz, elsőként arra kérünk, hozz létre Farm2Fork fiókot. A regisztráció során mondd el, hogy honnan hallottál rólunk és add hozzá a szállítási címedet és elérhetőségedet.
