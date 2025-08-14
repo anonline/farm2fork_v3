fba1ff5f6dff52bad7c101546e9005636a171e16
anonline <uerik26@gmail.com>
2025-08-14 18:16:17 +0200

    F2FV3-21| layout fix

--- src/app/dashboard/shipping/layout.tsx
+++ src/app/dashboard/shipping/layout.tsx
@@ -1,11 +1,6 @@
 'use client';
 
-import { paths } from 'src/routes/paths';
-
-import { DashboardContent } from 'src/layouts/dashboard';
 import { ShippingProvider } from 'src/contexts/shipping-context';
-
-import { CustomBreadcrumbs } from 'src/components/custom-breadcrumbs';
 
 import ShippingListView from 'src/sections/shipping/view/shipping-list-view';
 
@@ -15,16 +10,7 @@ import ShippingListView from 'src/sections/shipping/view/shipping-list-view';
 export default function Layout() {
   return (
     <ShippingProvider>
-      <DashboardContent maxWidth="xl">
-        <CustomBreadcrumbs
-          heading="Szállítási Zónák"
-          links={[
-            { name: 'Dashboard', href: paths.dashboard.root },
-            { name: 'Szállítási Zónák' },
-          ]}
-        />
         <ShippingListView />
-      </DashboardContent>
     </ShippingProvider>
   );
 }
