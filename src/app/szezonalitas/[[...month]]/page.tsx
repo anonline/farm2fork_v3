473072ae2615686ce4c6ceea89a5f9e166dfc415
anonline <uerik26@gmail.com>
2025-10-10 00:22:21 +0200

    feat: add seasonal page component with month handling and static params generation

--- src/app/szezonalitas/[[...month]]/page.tsx
+++ src/app/szezonalitas/[[...month]]/page.tsx
@@ -12,12 +12,23 @@ export const metadata: Metadata = { title: `Szezonalitás` };
 
 export default async function Page({ params }: Readonly<Props>) {
     const { month } = await params;
-
+    console.log(month);
+    
+    if(!month) {
+        const currentMonth = Object.values(MonthsEnum)[new Date().getMonth()] as MonthsEnum;
+        return <SzezonalitasView month={currentMonth} />;
+    }
+    
     const foundMonth = Object.values(MonthsEnum).find(
-        (m) => m.toLowerCase() === month.toLowerCase()
+        (m) => m.toLowerCase() === month[0].toLowerCase()
     );
-
     const selectedMonth = foundMonth ? (foundMonth as MonthsEnum) : MonthsEnum.January;
 
     return <SzezonalitasView month={selectedMonth} />;
+}
+
+export async function generateStaticParams() {
+  return Object.values(MonthsEnum).map((month) => ({
+    month: month.toLowerCase(),
+  }))
 }
